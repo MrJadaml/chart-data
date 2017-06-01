f6dddda0e0032eb4a36f5ba37d23dc4cbcccf169
mrjadaml <mrjadaml@gmail.com>
2017-05-31 19:45:52 -0700

    err handler middleware, monk/mongo, nodemon, /chartdata route

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const logger = require('morgan');
+const db = require('monk')('localhost/iunu');
+const Sensors = db.get('sensors');
 const app = express();
 
 app.use(logger('dev'));
 
-app.get('/', (_req, res, _next) => {
-  res.send('Hellow');
+app.get('/chartdata', (_req, res, _next) => {
+  res.json('hello world');
+});
+
+app.use((_req, _res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500)
+    .send({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500)
+  .send({
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;

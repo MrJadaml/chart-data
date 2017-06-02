f19e9a577c3cb21a51d4040e5a660581da07bf44
mrjadaml <mrjadaml@gmail.com>
2017-06-01 20:49:32 -0700

    basic endpoint tests and logic

--- app.js
+++ app.js
@@ -6,8 +6,64 @@ const app = express();
 
 app.use(logger('dev'));
 
-app.get('/chartdata', (_req, res, _next) => {
-  res.json('hello world');
+app.get('/chartdata', (req, res, next) => {
+  const { title, room, startDate, length } = req.query;
+
+  const tenMin = 600000
+  const seventyTwoHrs = 259200000;
+  const startPlusThreeDays = new Date(new Date(startDate).getTime() + seventyTwoHrs);
+  const endDate = length || startPlusThreeDays;
+
+  const tenMinSegments = (new Date(endDate).getTime() - new Date(startDate).getTime()) / tenMin;
+
+  if ( !title || !room || !startDate ) {
+    const err = new Error();
+    err.status = 400;
+    err.message = 'Missing room, start_date, or title';
+    return next(err);
+  }
+
+  Sensors.find({ title, room, timestamp: { $gte: new Date(startDate), $lt: new Date(endDate) } })
+  .then(data => {
+    const sortedData = data.sort((a,b) => new Date(a.timestamp, b.timestamp));
+    let index = 0;
+    let endOfTimeBlock = new Date(startDate).getTime() + tenMin;
+    let timeBlocks = [];
+    let charPoints = [];
+
+    sortedData.forEach(reading => {
+      if (new Date(reading.timestamp).getTime() > endOfTimeBlock) {
+        endOfTimeBlock += tenMin;
+        index++;
+      }
+
+      if (timeBlocks[index] instanceof Array) {
+        timeBlocks[index].push({
+          timestamp: new Date(endOfTimeBlock - tenMin),
+          value: reading.value,
+        });
+      } else {
+        timeBlocks[index] = [{
+          timestamp: new Date(endOfTimeBlock - tenMin),
+          value: reading.value,
+        }];
+      }
+    });
+
+    timeBlocks.forEach(set => {
+      let chartPoint;
+      let avg = 0;
+
+      set.forEach(reading => {
+        avg = avg + parseInt(reading.value)
+      });
+
+      chartPoint = {timestamp: set[0].timestamp, value: avg / set.length}
+      charPoints.push(chartPoint);
+    });
+
+    res.json(charPoints);
+  });
 });
 
 app.use((_req, _res, next) => {
@@ -19,7 +75,7 @@ app.use((_req, _res, next) => {
 if (app.get('env') === 'development') {
   app.use((err, _req, res, _next) => {
     res.status(err.status || 500)
-    .send({
+    .json({
       message: err.message,
       error: err
     });
@@ -28,7 +84,7 @@ if (app.get('env') === 'development') {
 
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500)
-  .send({
+  .json({
     message: err.message,
     error: {}
   });
---
+++ test/sensors.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const chai = require('chai')
+const should = chai.should();
+const request = require('supertest-as-promised');
+const app = require('../app');
+
+describe('/chartdata', () => {
+  it('should respond with a 400 if missing query params', done => {
+    request(app)
+      .get('/chartdata')
+      .expect(400)
+      .then(res => {
+        res.body.message.should.equal('Missing room, start_date, or title');
+        done();
+      })
+      //.catch(err => {
+        //console.log(err);
+        //done();
+      //})
+  });
+
+  it('should respond with timesamp and sensor value when given valid query params', done => {
+    request(app)
+      .get('/chartdata')
+      .query({title: 'temperature'})
+      .query({room: 'Hoop 1'})
+      .query({startDate: '2017-01-25T12:21:07.891z'})
+      .expect(200)
+      .then(res => {
+        res.body.should.be.an('array');
+        res.body.should.have.lengthOf.below(433);
+        res.body[0].should.have.all.keys('timestamp', 'value');
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      })
+  });
+});
